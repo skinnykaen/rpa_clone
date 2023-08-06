18040009b379e90cb49269aa6baf300059e7912a
Evgeniy Sushchevich <rupychman@mail.ru>
2023-08-06 21:12:07 +0300

    refactor: structure consts errors

--- internal/consts/errors.go
+++ internal/consts/errors.go
@@ -1,15 +1,28 @@
 package consts
 
+// http code 400
 const (
-	ErrEmailAlreadyInUse         = "email already in use"                         // http code 400
-	ErrAtoi                      = "string to int error"                          // 400
-	ErrActivationLinkUnavailable = "activation link is currently unavailable"     // 503
-	ErrIncorrectPasswordOrEmail  = "incorrect password or email"                  // 400
-	ErrUserIsNotActive           = "user is not active. please check your email"  // 403
-	ErrShortPassword             = "please input password, at least 6 symbols"    // 400
-	ErrTokenExpired              = "token expired"                                // 401
-	ErrNotStandardToken          = "token claims are not of type *StandardClaims" // 401
-	ErrProjectPageIsBanned       = "the projectPage is banned. no access"         // 403
-	ErrAccessDenied              = "access denied"                                // 403
-	ErrNotFoundInDB              = "not found"                                    // 400
+	ErrEmailAlreadyInUse        = "email already in use"
+	ErrAtoi                     = "string to int error"
+	ErrIncorrectPasswordOrEmail = "incorrect password or email"
+	ErrNotFoundInDB             = "not found"
+	ErrShortPassword            = "please input password, at least 6 symbols"
+)
+
+// http code 401
+const (
+	ErrTokenExpired     = "token expired"
+	ErrNotStandardToken = "token claims are not of type *StandardClaims"
+)
+
+// http code 403
+const (
+	ErrUserIsNotActive     = "user is not active. please check your email"
+	ErrProjectPageIsBanned = "the projectPage is banned. no access"
+	ErrAccessDenied        = "access denied"
+)
+
+// http code 503
+const (
+	ErrActivationLinkUnavailable = "activation link is currently unavailable"
 )
