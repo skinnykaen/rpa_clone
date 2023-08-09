fde146474d5ebc92804450cce7aa8cbba253b065
Evgeniy Sushchevich <rupychman@mail.ru>
2023-08-09 12:15:43 +0300

    fix: graphqlerr directive

--- internal/graphql/directives/directives.go
+++ internal/graphql/directives/directives.go
@@ -6,6 +6,7 @@ import (
 	"github.com/skinnykaen/rpa_clone/internal/consts"
 	"github.com/skinnykaen/rpa_clone/internal/models"
 	"github.com/skinnykaen/rpa_clone/pkg/utils"
+	"github.com/vektah/gqlparser/v2/gqlerror"
 	"log"
 	"net/http"
 )
@@ -15,9 +16,13 @@ func HasRole(errLogger *log.Logger) func(ctx context.Context, obj interface{}, n
 		clientRole := ctx.Value(consts.KeyRole)
 		if !utils.DoesHaveRole(clientRole.(models.Role), roles) {
 			errLogger.Printf("%s", consts.ErrAccessDenied)
-			return nil, utils.ResponseError{
-				Code:    http.StatusForbidden,
-				Message: consts.ErrAccessDenied,
+			return nil, &gqlerror.Error{
+				Extensions: map[string]interface{}{
+					"err": utils.ResponseError{
+						Code:    http.StatusForbidden,
+						Message: consts.ErrAccessDenied,
+					},
+				},
 			}
 		}
 		return next(ctx)
